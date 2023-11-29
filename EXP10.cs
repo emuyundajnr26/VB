0be0314a24a54ddd30b2e4235e7ace3dbcb51b21
emuyundajnr26 <108109300+emuyundajnr26@users.noreply.github.com>
2023-11-29 10:19:33 +0100

    Add files via upload

---
+++ EXP10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public int ProductID { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product { ProductID = 1, ProductName = "Product A", Price = 10.00m },
+            new Product { ProductID = 2, ProductName = "Product B", Price = 15.00m },
+            new Product { ProductID = 3, ProductName = "Product C", Price = 20.00m }
+        };
+
+        Console.WriteLine("Welcome to our Departmental Store");
+
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Product ID {product.ProductID}, Name {product.ProductName}, Price {product.Price}");
+        }
+    }
+}
